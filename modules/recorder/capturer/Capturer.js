022414387c87b4fa32fd37fe00d8d41bd6e04651
acoustic <acoustic@ki11er8ytes.ki11er8ytes.zone>
2020-11-12 22:12:05 -0500

    Stream frames directly to the emotions server

--- modules/recorder/capturer/Capturer.js
+++ modules/recorder/capturer/Capturer.js
@@ -21,27 +21,40 @@ class Capturer {
      */
     connect = async (dispatcherUrl) => {
         let capabilities = await this._capturer.getPhotoCapabilities();
-        let response = await fetch(`${dispatcherUrl}/job/${this._userId}`,
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                  },
-                body: JSON.stringify(capabilities), 
-                method:'post'
-            }
-        );
-        if (response.ok) { 
-            let json = await response.json();
-            this._socket = io(`https://${json.data.ip}:${json.data.port}`);
+        this._socket = io(dispatcherUrl);
+        this._socket.on('connect', () => {
+            console.log(`Send server-ping ${this._userId}`);
+            this._socket.emit('server-ping', this._userId);
+        });
+        this._socket.on('server-pong', data => {
+            console.log(`Received server-pong ${this._userId}`)
+            this._isLive = true;
+            this._socket.emit('add', this._userId);
+            this._pushNextFrame();
+        });
 
-            this._socket.on('connect', () => {
-                this._isLive = true;
-                this._pushNextFrame();
-            });
-        } 
-        else {
-            console.error(`Cannot establish connection with server: ${dispatcherUrl}`);
-        }
+        // let response = await fetch(`${dispatcherUrl}/emotion/${this._userId}`,
+        //     {
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //           },
+        //         body: JSON.stringify(capabilities), 
+        //         method:'post'
+        //     }
+        // );
+        // if (response.ok) { 
+        //     let json = await response.json();
+        //     this._socket = io(`https://${json.data.ip}:${json.data.port}`);
+
+        //     this._socket.on('connect', () => {
+        //         this._isLive = true;
+        //         this._socket.emit('add', this._userId);
+        //         this._pushNextFrame();
+        //     });
+        // } 
+        // else {
+        //     console.error(`Cannot establish connection with server: ${dispatcherUrl}`);
+        // }
     }
 
     /**
@@ -53,7 +66,7 @@ class Capturer {
         if (this._isLive) {
             try {
                 const blob = await this._capturer.takePhoto();
-                this._socket.emit('next-frame', blob);
+                this._socket.emit('next-frame', {id: this._userId, image: blob});
                 this._pushNextFrame(); // schedule next one
             } catch (err) {
                 console.error(err);
