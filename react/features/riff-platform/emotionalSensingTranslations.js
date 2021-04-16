8b54232bc09e7dd72427cbec48f923fd155cb1a0
Uliana Ostrova <uliana@riffanalytics.ai>
2021-04-16 16:08:16 +0300

    overwrite recording translations

---
+++ react/features/riff-platform/emotionalSensingTranslations.js
@@ -0,0 +1,35 @@
+/* eslint-disable max-len */
+export default {
+    dialog: {
+        recording: 'Emotional sensing',
+        recordingDisabledForGuestTooltip: 'Guests can\'t start emotional sensing.',
+        recordingDisabledTooltip: 'Start emotional sensing disabled.',
+        startRecording: 'Start emotional sensing',
+        stopRecording: 'Stop emotional sensing',
+        stopRecordingWarning: 'Are you sure you would like to stop the emotional sensing?'
+    },
+    recording: {
+        limitNotificationDescriptionWeb: 'Due to high demand your emotional sensing will be limited to {{limit}} min. For unlimited emotional sensing try <a href={{url}} rel=\'noopener noreferrer\' target=\'_blank\'>{{app}}</a>.',
+        limitNotificationDescriptionNative: 'Due to high demand your emotional sensing will be limited to {{limit}} min. For unlimited emotional sensings try <3>{{app}}</3>.',
+        availableSpace: 'Available space: {{spaceLeft}} MB (approximately {{duration}} minutes of emotional sensing)',
+        busy: 'We\'re working on freeing emotional sensing resources. Please try again in a few minutes.',
+        busyTitle: 'All emotional sensings are currently busy',
+        error: 'Emotional sensing failed. Please try again.',
+        expandedOff: 'Emotional sensing has stopped',
+        expandedOn: 'The meeting is currently being recorded.',
+        expandedPending: 'Emotional sensing is being started...',
+        failedToStart: 'Emotional sensing failed to start',
+        fileSharingdescription: 'Share emotional sensing with meeting participants',
+        off: 'Emotional sensing stopped',
+        offBy: '{{name}} stopped the emotional sensing',
+        on: 'Emotional sensing',
+        onBy: '{{name}} started the emotional sensing',
+        pending: 'Preparing to emotional sensing...',
+        rec: 'ES',
+        serviceDescription: 'Your emotional sensing will be saved by the emotional sensing service',
+        serviceName: 'Emotional sensing service',
+        unavailable: 'Oops! The {{serviceName}} is currently unavailable. We\'re working on resolving the issue. Please try again later.',
+        unavailableTitle: 'Emotional sensing unavailable'
+    },
+    toolbar: { accessibilityLabel: { recording: 'Toggle emotional sensing' } }
+};
